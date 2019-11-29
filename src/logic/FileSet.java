938f5e1d891341921f485b126b39e0d8df99adf5
LinkClink <shekaqwe@gmail.com>
2019-11-29 22:39:44 +0200

    Class (file state)

---
+++ src/logic/FileSet.java
@@ -0,0 +1,44 @@
+package logic;
+
+public class FileSet
+{
+    public String getFile_code() {
+        return file_code;
+    }
+
+    public void setFile_code(String file_code) {
+        this.file_code = file_code;
+    }
+
+    public String getBuffer_file() {
+        return buffer_file;
+    }
+
+    public void setBuffer_file(String buffer_file) {
+        this.buffer_file = buffer_file;
+    }
+
+    public String getFile_code_open() {
+        return file_code_open;
+    }
+
+    public void setFile_code_open(String file_code_open) {
+        this.file_code_open = file_code_open;
+    }
+
+    public String getFile_code_save() {
+        return file_code_save;
+    }
+
+    public void setFile_code_save(String file_code_save) {
+        this.file_code_save = file_code_save;
+    }
+
+    static String file_code;
+
+    static String buffer_file = null;
+
+    static String file_code_open = "Cp1251"; // ANSI
+    static String file_code_save = "Cp1251"; // ANSI
+
+}
