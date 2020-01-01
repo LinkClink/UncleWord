b05c53ec234804e1fdb43191c95da7775697c188
LinkClink <shekaqwe@gmail.com>
2020-01-01 12:26:45 +0200

    Update save functions

--- src/LogicJpanel/SaveAsTxT.java
+++ src/LogicJpanel/SaveAsTxT.java
@@ -1,34 +1,18 @@
 package LogicJpanel;
 
-import logic.FileSet;
-import logic.ShowErrorDialog;
+import logic.SaveFileFunctions;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.*;
 
 
 public class SaveAsTxT extends Component implements ActionListener
 {
     private JTextArea jTextArea;
-    private int result;
-    private String code_save;
-    private String buffer_file;
-    private String save_text;
 
-    FileSet fileSet = new FileSet();
-    ShowErrorDialog showErrorDialog = new ShowErrorDialog();
-
-    private Writer out;
-    private JFileChooser fileChooser = new JFileChooser();
-    private FileWriter fileWriter = null;
-
-    boolean filename_ck = true;
-    private int validate_flag;
-    private int validate_index;
-    private String validate;
+    SaveFileFunctions SaveFileFunctions = new SaveFileFunctions();
 
     public SaveAsTxT(JTextArea jTextArea)
     {
@@ -37,68 +21,6 @@ public class SaveAsTxT extends Component implements ActionListener
 
     @Override
     public void actionPerformed(ActionEvent e)
-    {
-       code_save = fileSet.getFile_code_save();
+    { SaveFileFunctions.SaveAs(jTextArea); }
 
-            if(jTextArea.getText()!=null)
-            {
-                result = fileChooser.showSaveDialog(SaveAsTxT.this);
-
-                if (fileChooser.getSelectedFile() != null) 
-                {
-
-                   validate = String.valueOf(fileChooser.getSelectedFile().getName());
-
-                    if(filename_ck == true) // ck file type
-                        buffer_file = String.valueOf(fileChooser.getSelectedFile()) + fileType_ck(validate);
-
-                    save_text = jTextArea.getText();
-
-                    try
-                    {
-                        fileWriter = new FileWriter(buffer_file);
-                    } catch (IOException e1)
-                    { showErrorDialog.show_dialog_0(e1.getMessage()); }
-                    try
-                    {
-                        out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(buffer_file), code_save));
-
-                    } catch (UnsupportedEncodingException e1)
-                    { showErrorDialog.show_dialog_0(e1.getMessage()); }
-                    catch (FileNotFoundException e1)
-                    { showErrorDialog.show_dialog_0(e1.getMessage()); }
-                    try
-                    {
-                        out.write(save_text);
-                    } catch (IOException e1)
-                    { showErrorDialog.show_dialog_0(e1.getMessage()); }
-                    finally
-                    {
-                        try
-                        {
-                            out.close();
-                        } catch (IOException e1)
-                        { showErrorDialog.show_dialog_0(e1.getMessage()); }
-                    }
-
-                }
-            }
-    }
-
-    public String fileType_ck(String name)
-    {
-        validate_flag=name.lastIndexOf(".txt");
-        validate_index=name.length();
-
-        if(validate_index-5 == validate_flag) validate_flag = -1; else validate_flag = 0;
-
-        switch (validate_flag)
-        {
-            case -1:
-            { return ".txt"; }
-
-            default:
-            { return ""; }
-        }
-    }
 }
--- src/LogicJpanel/SaveTxT.java
+++ src/LogicJpanel/SaveTxT.java
@@ -1,6 +1,7 @@
 package LogicJpanel;
 
 import logic.FileSet;
+import logic.SaveFileFunctions;
 import logic.ShowErrorDialog;
 
 import javax.swing.*;
@@ -12,54 +13,21 @@ public class SaveTxT implements ActionListener
 {
     private JTextArea jTextArea;
     private String buffer_file;
-    private String code_save;
-    private String save_text;
-    
-    FileWriter fileWriter = null;
-    Writer writer = null;
-    
+
     FileSet fileSet = new FileSet();
-    ShowErrorDialog showErrorDialog = new ShowErrorDialog();
+    SaveFileFunctions SaveFileFunctions = new SaveFileFunctions();
 
     public SaveTxT(JTextArea jTextArea)
-    {
-        this.jTextArea = jTextArea;
-    }
+    { this.jTextArea = jTextArea; }
 
     @Override
     public void actionPerformed(ActionEvent e) 
     {
-        code_save = fileSet.getFile_code_save();
         buffer_file = fileSet.getBuffer_file();
         
             if (buffer_file != null) 
-            {
-                save_text = jTextArea.getText();
-                try
-                {
-                    fileWriter = new FileWriter(buffer_file);
-                } catch (IOException e1)
-                { showErrorDialog.show_dialog_0(e1.getMessage()); }
-                try
-                {
-                    writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(buffer_file), code_save));
-                } catch (UnsupportedEncodingException e1)
-                { showErrorDialog.show_dialog_0(e1.getMessage());
-                } catch (FileNotFoundException e1)
-                { showErrorDialog.show_dialog_0(e1.getMessage()); }
-                try
-                {
-                    writer.write(save_text);
-                } catch (IOException e1)
-                { showErrorDialog.show_dialog_0(e1.getMessage()); }
-                finally
-                {
-                    try
-                    {
-                        writer.close();
-                    } catch (IOException e1)
-                    { showErrorDialog.show_dialog_0(e1.getMessage()); }
-                }
-            }
+            { SaveFileFunctions.Save(jTextArea); }
+            else
+            { SaveFileFunctions.SaveAs(jTextArea); }
     }
 }
